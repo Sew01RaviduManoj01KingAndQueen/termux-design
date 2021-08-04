82c28590b705c5fa9df5242423c5d62fae2fa52e
Ravindu-Manoj <86040559+Sew01RaviduManoj01KingAndQueen@users.noreply.github.com>
2021-08-04 12:13:55 +0530

    👑KingAndQueen❤Sew❤Ravindu👑

--- sew.sh
+++ sew.sh
@@ -4,7 +4,7 @@ echo ""
 read -p $' \e[0m\e[1;92m Input your Username : \e[0m\e[1;96m\en' user
 echo ""
 echo ""
-echo -e  $g "Please  Wait  A few Minutes...... Coded By Ravindu Manoj "
+echo -e  $g "Please  Wait  A few Minutes......"
 echo ""
 echo ""
 echo -e  $g "   
@@ -23,6 +23,10 @@ echo -e  $g "
             ╚══════╝╚══════╝ ╚══╝╚══╝       
                                             "
 echo ""
+echo ""
+echo -e  $g "Coded By t.me/RavinduManoj"
+echo ""
+echo ""
 pip install pyfiglet
 pip install termcolor
 pkg install ncurses-utils -y 
@@ -31,6 +35,5 @@ figlet -f big Wait | lolcat
 git clone https://github.com/Sew01RaviduManoj01KingAndQueen/simple-git-termux
 sed 's+THBD+'$user'+g' simple-git-termux/bash.bashrc > /data/data/com.termux/files/usr/etc/bash.bashrc
 sed 's+THBD+'$user'+g' simple-git-termux/wlc.py > /data/data/com.termux/files/usr/etc/wlc.py
-sed 's+THBD+'$user'+g' simple-git-termux/sew.py > /data/data/com.termux/files/usr/etc/sew.py
 rm -rf simple-git-termux
 figlet -f big BDh@Ckers | lolcat
